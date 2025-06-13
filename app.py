784e440713c5bddfe674540c2506ee4c32707df4
UnseenOFM <unseen.ofm@gmail.com>
2025-06-13 12:40:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, jsonify, request
 from names_dataset import NameDataset
 import random
 import string
-
+import re
 app = Flask(__name__)
 
+def is_latin(s):
+    return re.match(r'^[a-zA-Z]+$', s) is not None
+
 def generate_realistic_gmail(nd):
-    first_name = random.choice(list(nd.first_names.keys()))
-    last_name = random.choice(list(nd.last_names.keys()))
+    # Filtrer les prénoms et noms latins uniquement
+    latin_first_names = [name for name in nd.first_names.keys() if is_latin(name)]
+    latin_last_names = [name for name in nd.last_names.keys() if is_latin(name)]
+    first_name = random.choice(latin_first_names)
+    last_name = random.choice(latin_last_names)
     numbers = ''.join(random.choices(string.digits, k=random.randint(2, 3)))
     email = f"{first_name.lower()}.{last_name.lower()}{numbers}@gmail.com"
     return email
