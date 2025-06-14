977804a6111a624bf6642372ce8714464f9c707b
UnseenOFM <unseen.ofm@gmail.com>
2025-06-14 12:04:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,6 @@ def generate_gmails():
     return jsonify({"emails": gmails})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))  # Railway fournit automatiquement ce port
+    app.run(host='0.0.0.0', port=port)
