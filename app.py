f3644226fe1c779cfcb32807a495e02297419221
UnseenOFM <unseen.ofm@gmail.com>
2025-06-16 03:04:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,7 @@ def generate_gmails():
     return jsonify({"emails": gmails})
 
 if __name__ == '__main__':
-    print("[INFO] Lancement de l'application Flask sur 0.0.0.0:8000")
-    app.run(host='0.0.0.0', port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    print(f"[INFO] Lancement de l'application Flask sur 0.0.0.0:{port}")
+    app.run(host='0.0.0.0', port=port)
